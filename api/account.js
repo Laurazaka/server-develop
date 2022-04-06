3b805a494dab2d4e365db1c858d527ad393d54fa
Laurazaka <98113176+Laurazaka@users.noreply.github.com>
2022-04-06 21:16:01 +0300

    Default bad API response

--- api/account.js
+++ api/account.js
@@ -1,13 +1,43 @@
 const handler = {};
 
-handler.account = (data) => {
+handler.account = (data, callback) => {
     const acceptableMethods = ['get', 'post', 'put', 'delete'];
 
     if (acceptableMethods.includes(data.httpMethod)) {
-        console.log('Account: veiksmas leistinas');
-    } else {
-        console.log('Account: veiksmas NEleistinas');
+        return handler._method[data.httpMethod](data, callback);
     }
+
+    return callback(400, 'Account: veiksmas NEleistinas');
+}
+
+handler._method = {};
+
+handler._method.post = (data, callback) => {
+    return callback(200, {
+        action: 'POST',
+        msg: 'Vartotojo paskyra sukurta sekmingai',
+    })
+}
+
+handler._method.get = (data, callback) => {
+    return callback(200, {
+        action: 'GET',
+        msg: 'Stai tau visa info apie dominanti vartotoja',
+    })
+}
+
+handler._method.put = (data, callback) => {
+    return callback(200, {
+        action: 'PUT',
+        msg: 'Vartotojo informacija sekmingai atnaujinta',
+    })
+}
+
+handler._method.delete = (data, callback) => {
+    return callback(200, {
+        action: 'DELETE',
+        msg: 'Vartotojas sekmingai istrinta is sistemos',
+    })
 }
 
 export default handler;
